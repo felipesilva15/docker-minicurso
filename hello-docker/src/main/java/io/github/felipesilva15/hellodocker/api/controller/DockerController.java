5fe54c076d344773fef32994ae38e6eaf6a16416
Felipe Silva <felipe.allware@gmail.com>
2023-09-23 01:39:55 -0300

    Fix: Informando o

--- hello-docker/src/main/java/io/github/felipesilva15/hellodocker/api/controller/DockerController.java
+++ hello-docker/src/main/java/io/github/felipesilva15/hellodocker/api/controller/DockerController.java
@@ -1,6 +1,8 @@
 package io.github.felipesilva15.hellodocker.api.controller;
 
 import io.github.felipesilva15.hellodocker.domain.entity.HelloDocker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
+    Logger logger = LoggerFactory.getLogger(DockerController.class);
+
     @GetMapping("/hello-docker")
     public ResponseEntity<HelloDocker> hello() {
+        logger.info("Endpoint /hello-docker is caller!");
+
         String hostName = System.getenv("HOSTNAME");
 
         return new ResponseEntity<>(new HelloDocker("Hello Docker", hostName), HttpStatus.OK);
